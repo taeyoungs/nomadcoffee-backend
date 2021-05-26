5775ad1c3981573a0ed38400148793b32c8929a3
taeyoungs <xoxodudwkd@gmail.com>
2021-05-26 11:56:44 +0900

    Add createAccount mutation

--- src/users/users.mutations.ts
+++ src/users/users.mutations.ts
@@ -1,13 +1,59 @@
+import { Resolvers } from '../type';
 import client from '../client';
+import bcrypt from 'bcrypt';
 
-export default {
+interface IUserArgs {
+  username: string;
+  email: string;
+  password: string;
+  name?: string;
+  location?: string;
+}
+
+const resolvers: Resolvers = {
   Mutation: {
-    createUser: (_: any, { email, name }: { email: string; name?: string }) =>
-      client.user.create({
-        data: {
-          email,
-          name,
+    createAccount: async (
+      _,
+      { email, name, username, password, location }: IUserArgs
+    ) => {
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
         },
-      }),
+      });
+
+      if (existingUser) {
+        return {
+          ok: false,
+          error: 'username or email is already taken',
+        };
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await client.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+          name,
+          location,
+        },
+      });
+      // console.log(user);
+
+      return {
+        ok: true,
+      };
+    },
   },
 };
+
+export default resolvers;
--- src/users/users.typeDefs.ts
+++ src/users/users.typeDefs.ts
@@ -3,13 +3,30 @@ import { gql } from 'apollo-server';
 export default gql`
   type User {
     id: Int!
+    username: String!
     email: String!
     name: String
+    location: String
+    password: String!
+    avatarUrl: String
+    githubUsername: String
+    createAt: String!
+    updateAt: String!
+  }
+  type ReturnObj {
+    ok: Boolean!
+    error: String
   }
   type Query {
     user(id: Int!): User
   }
   type Mutation {
-    createUser(email: String!, name: String): User
+    createAccount(
+      username: String!
+      email: String!
+      name: String
+      location: String
+      password: String!
+    ): ReturnObj
   }
 `;
