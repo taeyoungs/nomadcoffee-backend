60ef2900bac76ce93f35d6cca460ebfeccb43ec0
taeyoungs <xoxodudwkd@gmail.com>
2021-06-09 23:45:14 +0900

    Add remove coffeeshop mutation

---
+++ src/coffeeShop/removeCoffeeSop/removeCoffeeShop.resolvers.ts
@@ -0,0 +1,43 @@
+import { Resolvers } from '../../type';
+import { protectedResolver } from '../../users/users.utils';
+
+export default {
+  Mutation: {
+    removeCoffeeShop: protectedResolver(async (_, { id }, { client }) => {
+      const shop = await client.coffeeShop.findUnique({
+        where: { id },
+      });
+      if (!shop) {
+        return {
+          ok: false,
+          error: '존재하지 않는 커피숍입니다.',
+        };
+      }
+
+      // cascade delete 미지원 => photo부터 삭제
+      try {
+        await client.coffeeShopPhoto.deleteMany({
+          where: {
+            shop: {
+              id,
+            },
+          },
+        });
+        await client.coffeeShop.delete({
+          where: {
+            id,
+          },
+        });
+
+        return {
+          ok: true,
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: `${error}`,
+        };
+      }
+    }),
+  },
+} as Resolvers;
